b8d0886e849a2e7ac5c35a1bc3ced92bc54daa42
Alper Ceyhun <80386511+AlperCeyhun@users.noreply.github.com>
2025-06-17 10:53:52 +0300

    added dummy fetch for testing

---
+++ src/lib/fetchtodo.ts
@@ -0,0 +1,17 @@
+export type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+export async function fetchTodos(): Promise<Todo[]> {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch todos. Status: ${res.status}`);
+  }
+
+  const data: Todo[] = await res.json();
+  return data.slice(0, 10); // Just return the first 10 todos
+}
