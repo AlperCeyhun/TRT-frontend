12bd6ad6ddba33959b451ab72cf3ce16fa3bbd87
Alper Ceyhun <80386511+AlperCeyhun@users.noreply.github.com>
2025-06-18 16:51:44 +0300

    changed routes to '/home' from '/' and added todoDataCardTop to datatable page

--- src/app/datacard/page.tsx
+++ src/app/datacard/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const router = useRouter();
   
   const handleBack = () => {
-    router.push("/");
+    router.push("/home");
   };
   
   return (
--- src/app/datatable/page.tsx
+++ src/app/datatable/page.tsx
@@ -4,13 +4,19 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { ArrowBigLeft } from "lucide-react";
+import TodoDatacardTop from "@/components/TodoDatacardTop";
 
 export default function Home() {
   
   const router = useRouter();
-    
+  const [newTask, setNewTask] = React.useState<string>("");
+  
+  const handleAddTask = () => {
+    //some logic here
+  }
+  
   const handleBack = () => {
-    router.push("/");
+    router.push("/home");
   };
   
 	return (
@@ -19,6 +25,7 @@ export default function Home() {
             <ArrowBigLeft className="mr-2" />
             Back to Home
         </Button>
+        <TodoDatacardTop newTask={newTask} setNewTask={setNewTask} handleAddTask={handleAddTask} />
     </div>
   );
 }
