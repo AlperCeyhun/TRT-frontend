4207a478a27329fa078f132f880ae0dbb1373f7f
Alper Ceyhun <80386511+AlperCeyhun@users.noreply.github.com>
2025-06-20 10:44:34 +0300

    added getusers.ts fetchcall

---
+++ src/lib/user/getusers.ts
@@ -0,0 +1,22 @@
+export type User = {
+  id: number;
+  username: string;
+  password: string;
+  assignees: any; // Adjust the type
+};
+
+export async function getUsers(): Promise<User[]> {
+  const res = await fetch("http://localhost:5195/api/user", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch users. Status: ${res.status}`);
+  }
+
+  const data = await res.json();
+  return data as User[];
+}
